1c1a3aa50b3ae7a9252f4208220046180256984b
Elisio <racabral11@icloud.com>
2024-11-20 21:34:45 +0000

    added online user green point on OnlineUser.js components

--- src/components/OnlineUser.js
+++ src/components/OnlineUser.js
@@ -9,7 +9,8 @@ export default function OnlineUser() {
             <h2>All users</h2>
             {error && <div className="error">{error}</div>}
             {documents && documents.map(user => (
-                <div key={user.id} className="user">
+                <div key={user.id} className="online-user-item">
+                    {user.online && <span className="online-user"></span>}
                     <span>{user.displayName}</span>
                     <Avatar src={user.photoURL} />
                 </div>
