0f88b0d4db835b63b9a00c3ee124b2e82e1c808b
Elisio <racabral11@icloud.com>
2024-11-20 19:19:25 +0000

    implemented signup.js page and respective states

--- src/pages/Signup/signup.js
+++ src/pages/Signup/signup.js
@@ -1,9 +1,73 @@
 import './signup.css';
+import {useState} from "react";
 
 export default function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const [thumbnail, setThumbnail] = useState(null);
+    const [thumbnailError, setThumbnailError] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, password, displayName, thumbnail);
+    }
+    const handleFileChange = (e) => {
+        setThumbnail(null);
+        let selected = e.target.files[0];
+        if (!selected) {
+            setThumbnailError('Please select a file');
+            return;
+        }
+        if (!selected.type.includes('image')) {
+            setThumbnailError('Please select an image file');
+            return;
+        }
+        if (selected.size > 1000000) {
+            setThumbnailError('File size is too large');
+            return;
+        }
+        setThumbnailError(null);
+        setThumbnail(selected);
+        console.log('thumbnail selected:');
+    }
+
     return (
-        <div className="signup">
-        <h1>Signup</h1>
-        </div>
+        <form className="auth-form" onSubmit={handleSubmit}>
+            <h2>Sign up</h2>
+            <label>
+                <span>email: </span>
+                <input
+                    required
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <span>password: </span>
+                <input
+                    required
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <span>profile thumbnail: </span>
+                <input
+                    required
+                    type="displayName"
+                    value={displayName}
+                    onChange={e => setDisplayName(e.target.value)}
+                />
+            </label>
+            <label>
+                <span>profile thumbnail:</span>
+                <input
+                    required
+                    type="file"
+                    onChange={handleFileChange}
+                />
+                {thumbnailError && <p className="error">{thumbnailError}</p>}
+            </label>
+            <button className="btn" type="submit">Sign up</button>
+        </form>
     );
 }
