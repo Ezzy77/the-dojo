c6d328568c64d512222f31c45b6a964e975d4e98
Elisio <racabral11@icloud.com>
2024-11-20 20:23:32 +0000

    implemented logging out users and setting online status to false

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
 import './NavBar.css'
 import Temple from '../assets/temple.svg'
 import {Link} from "react-router-dom";
+import {useLogout} from "../hooks/useLogout";
 
 export default function NavBar() {
+    const {logout, isPending, error} = useLogout();
     return(
         <div className="navbar">
             <ul>
@@ -12,7 +14,10 @@ export default function NavBar() {
                 </li>
                 <li><Link to="/login"></Link></li>
                 <li><Link to="/signup">Signup</Link></li>
-                <li><button className="btn">Logout</button></li>
+                <li>
+                    {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                    {isPending && <button className="btn" onClick={logout} disabled>Logging out...</button>}
+                </li>
             </ul>
         </div>
     )
